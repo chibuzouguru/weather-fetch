aa8dfd630b80d8253818c0478c497283c7a45745
Chibuzo Uguru <chibuzo.uguru@gmail.com>
2019-02-19 14:17:13 -0800

    app changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Changing to check github functionality.
+            Changing to check github functionality
           </p>
           <a
             className="App-link"
